b062c82f46c6f1b01ee9cfd3daec18b2b591c5c1
Emily R <emilyroulund@gmail.com>
2019-05-07 00:03:58 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,55 @@
+#{seasons:[{contestants}] contestants= {stats:values}
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants| #array of contestants
+    if contestants["status"] == "Winner"
+          return contestants["name"].split.first 
+        end
+      end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |info|
+    if info["occupation"] == occupation 
+      return info["name"]
+    end 
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.each do |season, contestants|
+    contestants.each do |info|
+      if info["hometown"] == hometown
+        i += 1 
+      end
+      end
+    end
+    i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |info|
+    if info["hometown"] == hometown
+      return info["occupation"]
+    end
+    end
+  end
 end
 
+
+
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0
+  i = 0
+  data[season].each do |contestants|
+    contestants.each do |info, values|
+      i += 1
+      total_age += contestants["age"].to_f
+    end
+  end
+    (total_age/i).round
 end
